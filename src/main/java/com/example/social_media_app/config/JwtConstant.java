bed667d79c965ac9ec78dbc567316b3431bc242f
Ramia-aloufi <ramia.aloufi@gmail.com>
2025-06-26 01:18:31 +0300

    add some values to properties

--- src/main/java/com/example/social_media_app/config/JwtConstant.java
+++ src/main/java/com/example/social_media_app/config/JwtConstant.java
@@ -1,5 +1,8 @@
 package com.example.social_media_app.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConstant  {
     public static final String SECRET_KEY = "aW5zZWN1cmUtc2VjcmV0LXN0cmluZy1mb3ItanNvbg==";
     public static final String JWT_HEADER = "Authorization";
